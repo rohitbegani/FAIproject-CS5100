d5194fd5ad2b523b2d2cb0b906404b0eb9a33f3e
Bhanu Pratap Jain <bhanupratapjain@ccs.neu.edu>
2015-12-03 17:28:19 -0500

    Adding Data Mapping

--- models/business.py
+++ models/business.py
@@ -1,5 +1,20 @@
 class Business(object):
-
-    def __init__(self, name=None, id=None):
+    def __init__(self, name=None, business_id=None):
         self.name = name
-        self.id = id
+        self.business_id = business_id
+
+    @property
+    def name(self):
+        return self.name
+
+    @name.setter
+    def name(self, name):
+        self.name = name
+
+    @property
+    def business_id(self):
+        return self.business_id
+
+    @business_id.setter
+    def business_id(self, business_id):
+        self.business_id = business_id
